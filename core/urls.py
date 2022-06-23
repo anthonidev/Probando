6a42dbdf37527cf4687cb70794a0b14a4dd776b6
anthonidev <softwaretoni21@gmail.com>
2022-06-23 11:22:55 -0500

    aton-backend-django

---
+++ core/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path, include
+from django.contrib import admin
+
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
+# from rest_framework import permissions
+
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="ATON API",
+#         default_version='v1',
+#         description="Test description",
+#         terms_of_service="https://www.google.com/policies/terms/",
+#         contact=openapi.Contact(email="contact@snippets.local"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     permission_classes=[permissions.AllowAny],
+# )
+
+
+urlpatterns = [
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.social.urls')),
+    # path('api/product/', include('apps.product.urls')),
+    # path('api/account/', include('apps.account.urls')),
+    # path('api/cart/', include('apps.cart.urls')),
+    # path('api/wishlist/', include('apps.wishlist.urls')),
+    # path('api/shipping/', include('apps.shipping.urls')),
+    # path('api/coupon/', include('apps.coupon.urls')),
+    # path('api/order/', include('apps.order.urls')),
+    # path('docs/', schema_view.with_ui('swagger', cache_timeout=0),
+    #      name='schema-swagger-ui'),
+    path('', admin.site.urls),
+]
